0ea9ba14614736c829b80f0857b4ee4b9fef8ad9
Fiza Amir <fizaa8123@gmail.com>
2024-03-03 11:38:50 +0500

    first commit

--- string.ts
+++ string.ts
@@ -1,10 +1,12 @@
-let fullname: string = "shah";
+let fullname: string = "shah";//string example
 console.log(fullname)
-let message: number = 123;
+let message: number = 123;//number example
 console.log(message)
-let isDone: boolean = false;
+let isDone: boolean = false;//boolean example
 console.log(isDone)
-let noTsure: any = 10;
+let noTsure: any = 10;//any example
 console.log(noTsure)
 let mYnAme: number = 167;
 console.log(mYnAme)
+let mytRun: boolean = true;//boolean example
+console.log(mytRun)
