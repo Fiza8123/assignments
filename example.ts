460143adf2663e5aef6f415ac6e1f07fc6ab6451
Fiza Amir <fizaa8123@gmail.com>
2024-03-03 14:20:09 +0500

    first commit

---
+++ example.ts
@@ -0,0 +1,23 @@
+let fullnAme: string = "ahad";
+console.log(fullnAme)
+// example of  strong string
+let numbers : number =345662;
+console.log(numbers)
+//example ofstrong number
+let soMesoMe: boolean = false;
+console.log(soMesoMe)
+//example of strong boolean 
+let inDone : any = 123;
+console.log(inDone)
+//example of strong any 
+let mYnamE ="fiza amir";
+console.log(mYnamE)
+//example of string 
+let numerical = 234;
+console.log(numerical)
+//example of number
+let boolean =true;
+console.log(boolean)
+//example of boolean
+
+
