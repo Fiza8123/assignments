d4248b5476dc2a1eae3c3af8707e2557fd1e3e6c
Fiza Amir <fizaa8123@gmail.com>
2024-03-06 18:44:55 +0500

    first commit

---
+++ LEARNING.js
@@ -0,0 +1,69 @@
+var fullnAme = "ahad";
+console.log(fullnAme);
+// example of  strong string
+var numbers = 345662;
+console.log(numbers);
+//example ofstrong number
+var soMesoMe = false;
+console.log(soMesoMe);
+//example of strong boolean 
+var inDone = 123;
+console.log(inDone);
+//example of strong any 
+var mYnamE = "fiza amir";
+console.log(mYnamE);
+//example of string 
+var numerical = 234;
+console.log(numerical);
+//example of number
+var boolean = true;
+console.log(boolean);
+//example of boolean
+// types of writting
+// cameLCase
+//snake_case
+//PascalCase
+var userName = "fizza"; // e.g. camelCase
+var user_name = "fatima"; //e.g. snake_case
+var UserName = "bisma"; //e.g. PascalCase
+var isUserExist = true;
+var is_user_exist = false;
+var IsUserExist = 123;
+var anyType = 
+// = is assigment operator
+console.log(userName, user_name, UserName);
+console.log(isUserExist, is_user_exist, IsUserExist);
+console.log(anyType);
+//arrays=> []
+//=> 0,..... 
+//array address in list
+var students = ["Noman", "Ali", "ahad", 1, 10, true];
+var firststudentName = students[0];
+console.log(firststudentName);
+console.log(students);
+console.log(students[5]);
+console.log(students[3]);
+// if we wonna exist one name in list
+var friends = ["fiza", "saba", "sana"];
+console.log(friends);
+var student = ["wassem"];
+console.log(student[0]);
+//example that who can we make array conform to accept only string . it can also work in any type of types of writing in typescript
+var numberArray = [1, 2, 3, 4];
+console.log(numberArray);
+//example of array of number
+var booleanArray = [true, false];
+console.log(booleanArray);
+//example of array of boolean 
+var zuserName = "ali";
+zuserName = "ahmed";
+console.log(zuserName);
+//do it in array
+//string in example:
+var fruits = ["apple", "banana", "orange"];
+fruits[1] = "grapes";
+console.log(fruits);
+//boolean in example:
+var game = [true, false];
+game[0] = false;
+console.log(game);
---
+++ LEARNING.ts
@@ -0,0 +1,118 @@
+let fullnAme: string = "ahad";
+console.log(fullnAme)
+
+// example of  strong string
+
+let numbers : number =345662;
+console.log(numbers)
+
+//example ofstrong number
+
+let soMesoMe: boolean = false;
+console.log(soMesoMe)
+
+//example of strong boolean 
+
+let inDone : any = 123;
+console.log(inDone)
+
+//example of strong any 
+
+let mYnamE ="fiza amir";
+console.log(mYnamE)
+
+//example of string 
+
+let numerical = 234;
+console.log(numerical)
+
+//example of number
+
+let boolean =true;
+console.log(boolean)
+
+//example of boolean
+
+
+              // types of writting
+// cameLCase
+//snake_case
+//PascalCase
+
+let userName: string = "fizza"// e.g. camelCase
+let user_name: string  = "fatima" //e.g. snake_case
+let UserName :string = "bisma" //e.g. PascalCase
+
+
+let isUserExist: boolean  = true;
+let is_user_exist: boolean  = false;
+let IsUserExist: number = 123; 
+let anyType =
+
+
+// = is assigment operator
+console.log(userName, user_name, UserName)
+console.log(isUserExist, is_user_exist, IsUserExist)
+console.log(anyType)
+
+
+//arrays=> []
+//=> 0,..... 
+//array address in list
+
+let students = ["Noman", "Ali", "ahad",1,10,true]
+let firststudentName = students[0] 
+console.log(firststudentName)
+
+console.log (students)
+console.log(students[5])
+console.log(students[3])
+
+// if we wonna exist one name in list
+let friends = ["fiza", "saba", "sana"]
+console.log(friends)
+
+let student: string[] = ["wassem"]
+console.log(student[0])
+//example that who can we make array conform to accept only string . it can also work in any type of types of writing in typescript
+
+let numberArray: number[] = [1,2,3,4]
+console.log(numberArray)
+
+//example of array of number
+
+let booleanArray: boolean[] = [true,false]
+console.log(booleanArray)
+
+//example of array of boolean 
+
+let zuserName: string = "ali"
+zuserName = "ahmed"
+console.log(zuserName)
+
+
+//do it in array
+
+     //string in example:
+
+let fruits: string[] =["apple", "banana", "orange"]
+fruits[1] = "grapes"
+console.log(fruits)
+
+     //boolean in example:
+
+let game: boolean[] =[true,false]
+game[0] = false
+console.log(game)
+
+
+
+
+
+
+
+
+
+
+
+
